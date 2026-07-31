b1ce5a7bf2427a1368d65a930c7067d2061e80b3
excellogalihs <excellogalihs@gmail.com>
2026-07-31 21:42:09 +0700

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo pacman -S hyprland hyprpaper hyprpolkitagent xdg-desktop-portal-hyprland sddm pipewire pipewire-pulse wireplumber kitty zsh-syntax-highlighting zsh-autosuggestions starship waybar swaync wofi firefox ttf-jetbrains-mono-nerd grim wl-clipboard nvim yazi fzf bat zoxide eza fastfetch
-cp -f ~/friedrice/.zshrc ~
+cp -f ~/friedrice/.zshrc ~/
+cp -f ~/friedrice/.config/starship.toml ~/.config/
 rm -rf ~/.config/{fastfetch,hypr,waybar,wofi}
 cp -r ~/friedrice/.config/{fastfetch,hypr,waybar,wofi} ~/.config/
 reboot
