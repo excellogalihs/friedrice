f7859ceccec178487b93144621d3cdaf6db9105c
excellogalihs <excellogalihs@gmail.com>
2026-07-31 14:35:39 +0700

    Adding prerequisites

--- copy.sh
+++ copy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo pacman -S hyprland hyprpaper hyprpolkitagent xdg-desktop-portal-hyprland sddm pipewire pipewire-pulse wireplumber kitty zsh-syntax-highlighting zsh-autosuggestions starship waybar swaync wofi firefox ttf-jetbrains-mono-nerd grim wl-clipboard nvim yazi fzf bat zoxide eza fastfetch
 cp -f ~/friedrice/.zshrc ~
 rm -rf ~/.config/{fastfetch,hypr,waybar,wofi}
 cp -r ~/friedrice/.config/{fastfetch,hypr,waybar,wofi} ~/.config/
