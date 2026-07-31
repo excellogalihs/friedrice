a3d3bce909fbd6074d96d341c0f91614fc628a42
excellogalihs <excellogalihs@gmail.com>
2026-07-31 14:41:28 +0700

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -3,4 +3,4 @@ sudo pacman -S hyprland hyprpaper hyprpolkitagent xdg-desktop-portal-hyprland sd
 cp -f ~/friedrice/.zshrc ~
 rm -rf ~/.config/{fastfetch,hypr,waybar,wofi}
 cp -r ~/friedrice/.config/{fastfetch,hypr,waybar,wofi} ~/.config/
-hyprctl dispatch exit
+reboot
