35a1219cb7fe75b5ac950b991b8fb77d16ee571f
excellogalihs <excellogalihs@gmail.com>
2026-08-01 17:39:23 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
 set -e
-
-echo "Initializing friedrice setup by excellogalihs..."
-
-echo "Installing packages..."
 sudo pacman -S --noconfirm git base-devel pipewire pipewire-pulse wireplumber hyprland hyprpaper hyprpolkitagent xdg-desktop-portal-hyprland sddm kitty waybar wofi swaync zsh zsh-autosuggestions zsh-syntax-highlighting starship nvim yazi fzf bat zoxide eza fastfetch grim wl-clipboard firefox ttf-jetbrains-mono-nerd
-
-echo "Configuring shell..."
 chsh -s $(which zsh)
-
-echo "Installing yay..."
 rm -rf yay
 git clone https://aur.archlinux.org/yay
 cd yay
 makepkg -si --noconfirm
-
-echo "Copying configs..."
 cp -f ~/friedrice/.zshrc ~/
 cp -f ~/friedrice/.config/starship.toml ~/.config/
 rm -rf ~/.config/{fastfetch,hypr,kitty,waybar,wofi}
 cp -r ~/friedrice/.config/{fastfetch,hypr,kitty,waybar,wofi} ~/.config/
-
-echo "Installation complete! Rebooting..."
 reboot
