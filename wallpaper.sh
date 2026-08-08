3c324067ae20597e24170a2882b5ede9bef6ec56
excellogalihs <excellogalihs@gmail.com>
2026-08-08 19:12:52 +0700

    Just some tweaks

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 choice=$(ls ~/friedrice/wallpapers/ | wofi --dmenu --prompt "Wallpaper:")
-awww img ~/friedrice/wallpapers/"$choice" --transition-type grow --transition-duration 3
+awww img ~/friedrice/wallpapers/$choice --transition-type grow --transition-duration 3
+rm .config/hypr/hyprlock.conf
+touch .config/hypr/hyprlock.conf
+echo "
+background {
+  path = ~/friedrice/wallpapers/$choice
+  blur_size = 3
+  blur_passes = 3
+}
+input-field {
+  size = 300, 50
+}
+" >.config/hypr/hyprlock.conf
