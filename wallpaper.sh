4b7aa152d27e376431c85956fae65b1b9ce394ea
excellogalihs <excellogalihs@gmail.com>
2026-08-09 09:35:31 +0700

    Just some tweaks

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 choice=$(ls ~/friedrice/wallpapers/ | wofi --dmenu --prompt "Wallpaper:")
 awww img ~/friedrice/wallpapers/$choice --transition-type random --transition-duration 3
-rm .config/hypr/hyprlock.conf
-touch .config/hypr/hyprlock.conf
 echo "
 background {
   path = ~/friedrice/wallpapers/$choice
