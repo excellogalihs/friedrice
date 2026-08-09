af3945a206b1bebff7e9c6f4319030cfae5ac900
excellogalihs <excellogalihs@gmail.com>
2026-08-09 16:51:45 +0700

    Just some tweaks

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 git clone https://github.com/excellogalihs/friedrice
 cd friedrice
 chmod +x setup.sh
-chmod +x switch.sh
+chmod +x wallpaper.sh
 ./setup.sh
--- switch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-choice=$(printf "dynamic\nfull" | wofi --dmenu --prompt "Waybar Layout:")
-ln -sf ~/.config/waybar/layouts/$choice/config.jsonc ~/.config/waybar/config.jsonc
-ln -sf ~/.config/waybar/layouts/$choice/style.css ~/.config/waybar/style.css
-killall waybar
-waybar
