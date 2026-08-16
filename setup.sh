d46c9167d733ad5fd08c18b6fc9e8b32569a6701
excellogalihs <excellogalihs@gmail.com>
2026-08-16 10:17:25 +0700

    A few tweaks

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-sudo pacman -S --noconfirm git base-devel pipewire pipewire-pulse wireplumber pavucontrol hyprland hyprlock hyprpolkitagent xdg-desktop-portal-hyprland sddm kitty zsh zsh-autosuggestions zsh-syntax-highlighting starship awww waybar wofi power-profiles-daemon nvim yazi fzf ripgrep fd bat zoxide eza tree-sitter-cli fastfetch grim wl-clipboard firefox ttf-jetbrains-mono-nerd
+sudo pacman -S --noconfirm git base-devel pipewire pipewire-pulse wireplumber pavucontrol hyprland hyprlock hyprpolkitagent xdg-desktop-portal-hyprland sddm kitty zsh zsh-autosuggestions zsh-syntax-highlighting starship awww waybar wlogout wofi power-profiles-daemon nvim yazi fzf ripgrep fd bat zoxide eza tree-sitter-cli fastfetch grim wl-clipboard firefox ttf-jetbrains-mono-nerd
 rm -rf yay
 git clone https://aur.archlinux.org/yay
 cd yay
@@ -9,7 +9,7 @@ yay -S --noconfirm hyprmod
 chsh -s $(which zsh)
 cp -f ~/friedrice/.zshrc ~/
 cp -f ~/friedrice/.config/starship.toml ~/.config/
-rm -rf ~/.config/{fastfetch,hypr,kitty,nvim,waybar,wofi}
-cp -r ~/friedrice/.config/{fastfetch,hypr,kitty,nvim,waybar,wofi} ~/.config/
+rm -rf ~/.config/{fastfetch,hypr,kitty,nvim,waybar,wlogout,wofi}
+cp -r ~/friedrice/.config/{fastfetch,hypr,kitty,nvim,waybar,wlogout,wofi} ~/.config/
 awww img ~/friedrice/wallpapers/astronaut.jpg
 reboot
